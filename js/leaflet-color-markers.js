fb5172c1eed599da3f2b50ae3d9a6db2787d8327
r3dlin3 <r3dlin3@users.noreply.github.com>
2017-12-05 13:59:26 +0100

    Our own icon class containing the shared options
    cf. http://leafletjs.com/examples/custom-icons/#defining-an-icon-class

--- js/leaflet-color-markers.js
+++ js/leaflet-color-markers.js
@@ -1,71 +1,40 @@
-var blueIcon = new L.Icon({
-	iconUrl: 'img/marker-icon-2x-blue.png',
-	shadowUrl: 'img/marker-shadow.png',
-	iconSize: [25, 41],
-	iconAnchor: [12, 41],
-	popupAnchor: [1, -34],
-	shadowSize: [41, 41]
+var colorIcon = L.Icon.extend({
+	options: {
+		shadowUrl: 'https://cdn.rawgit.com/r3dlin3/leaflet-color-markers/master/img/marker-shadow.png',
+		iconSize: [25, 41],
+		iconAnchor: [12, 41],
+		popupAnchor: [1, -34],
+		shadowSize: [41, 41]
+	}
+});
+var blueIcon = new colorIcon({
+	iconUrl: 'https://cdn.rawgit.com/r3dlin3/leaflet-color-markers/master/img/marker-icon-blue.png',
 });
 
-var redIcon = new L.Icon({
-	iconUrl: 'img/marker-icon-2x-red.png',
-	shadowUrl: 'img/marker-shadow.png',
-	iconSize: [25, 41],
-	iconAnchor: [12, 41],
-	popupAnchor: [1, -34],
-	shadowSize: [41, 41]
+var redIcon = new colorIcon({
+	iconUrl: 'https://cdn.rawgit.com/r3dlin3/leaflet-color-markers/master/img/marker-icon-red.png',
 });
 
-var greenIcon = new L.Icon({
-	iconUrl: 'img/marker-icon-2x-green.png',
-	shadowUrl: 'img/marker-shadow.png',
-	iconSize: [25, 41],
-	iconAnchor: [12, 41],
-	popupAnchor: [1, -34],
-	shadowSize: [41, 41]
+var greenIcon = new colorIcon({
+	iconUrl: 'https://cdn.rawgit.com/r3dlin3/leaflet-color-markers/master/img/marker-icon-green.png',
 });
 
-var orangeIcon = new L.Icon({
-	iconUrl: 'img/marker-icon-2x-orange.png',
-	shadowUrl: 'img/marker-shadow.png',
-	iconSize: [25, 41],
-	iconAnchor: [12, 41],
-	popupAnchor: [1, -34],
-	shadowSize: [41, 41]
+var orangeIcon = new colorIcon({
+	iconUrl: 'https://cdn.rawgit.com/r3dlin3/leaflet-color-markers/master/img/marker-icon-orange.png',
 });
 
-var yellowIcon = new L.Icon({
-	iconUrl: 'img/marker-icon-2x-yellow.png',
-	shadowUrl: 'img/marker-shadow.png',
-	iconSize: [25, 41],
-	iconAnchor: [12, 41],
-	popupAnchor: [1, -34],
-	shadowSize: [41, 41]
+var yellowIcon = new colorIcon({
+	iconUrl: 'https://cdn.rawgit.com/r3dlin3/leaflet-color-markers/master/img/marker-icon-yellow.png',
 });
 
-var violetIcon = new L.Icon({
-	iconUrl: 'img/marker-icon-2x-violet.png',
-	shadowUrl: 'img/marker-shadow.png',
-	iconSize: [25, 41],
-	iconAnchor: [12, 41],
-	popupAnchor: [1, -34],
-	shadowSize: [41, 41]
+var violetIcon = new colorIcon({
+	iconUrl: 'https://cdn.rawgit.com/r3dlin3/leaflet-color-markers/master/img/marker-icon-violet.png',
 });
 
-var greyIcon = new L.Icon({
-	iconUrl: 'img/marker-icon-2x-grey.png',
-	shadowUrl: 'img/marker-shadow.png',
-	iconSize: [25, 41],
-	iconAnchor: [12, 41],
-	popupAnchor: [1, -34],
-	shadowSize: [41, 41]
+var greyIcon = new colorIcon({
+	conUrl: 'https://cdn.rawgit.com/r3dlin3/leaflet-color-markers/master/img/marker-icon-grey.png',
 });
 
-var blackIcon = new L.Icon({
-	iconUrl: 'img/marker-icon-2x-black.png',
-	shadowUrl: 'img/marker-shadow.png',
-	iconSize: [25, 41],
-	iconAnchor: [12, 41],
-	popupAnchor: [1, -34],
-	shadowSize: [41, 41]
+var blackIcon = new colorIcon({
+	iconUrl: 'https://cdn.rawgit.com/r3dlin3/leaflet-color-markers/master/img/marker-icon-black.png',
 });
